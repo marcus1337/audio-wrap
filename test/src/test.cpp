aca6ea087f0e9bc7022aefbacea528314ce4a713
marcus1337 <blurgas@hotmail.com>
2023-03-11 09:38:30 +0100

    test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -5,6 +5,9 @@
 #include <filesystem>
 #include <iostream>
 
+#include <chrono>
+#include <thread>
+
 std::vector<std::string> getResourceFolderPaths() {
     std::vector<std::string> paths;
     std::string resourceFolderPath = std::filesystem::current_path().string() + "//res//";
@@ -16,6 +19,19 @@ std::vector<std::string> getResourceFolderPaths() {
 int main(int argc, char* argv[]) {
 
     Audio audio;
+    auto soundFolders = getResourceFolderPaths();
+    auto fxFolder = soundFolders[0];
+    auto musicFolder = soundFolders[1];
+    std::string pianoWavPath = musicFolder + "sample.wav";
+
+    audio.unmuteSound();
+    audio.loadSounds({ pianoWavPath }, { "piano" });
+    audio.playMusic("piano");
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    
 
     return 0;
 }
